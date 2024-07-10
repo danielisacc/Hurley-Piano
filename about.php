da93ebed9a8ea118818028d2421705585a7a4df3
lethienkim <131014498+lethienkim@users.noreply.github.com>
2024-07-09 19:28:22 -0500

    add the about.php, about.css, contact.php, contact.css

---
+++ about.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hurley Piano Website - About</title>
+    <link rel="icon" type="img/png" href="../img/favicon.png">
+    <!--
+    <link rel="stylesheet" href="about.css">
+    -->
+
+
+    
+</head>
+<body>
+    <?php include("header.html"); ?>
+    <main>
+        <!-- Main content goes here -->
+        <div class="centered-tile-full-width-hero">
+            <h1>About</h1>
+            <h3>HURLEY PIANO</h3>
+            <img src="../img/about/hero.png" alt="Teaching musical instruments to Neurodiverse Kids" class="hero-photo">
+            <p>Hurley Piano develops education methodologies for easy learning to help parents teach their kids music...</p>
+            <br>
+        </div>
+        <div class="wide-image">
+            <img src="../img/about/piano.png" alt="child and adult playing piano together">
+        </div>
+        <div class="testimonial-box">
+            <h2>My Mission</h2><br>
+            <img src="../img/about/sheet-music.png" alt="fingers on sheet music">
+            <p>The Piano serves as a powerful educational tool for neurodiverse children...</p><br><br>
+        </div>
+        <div class="star-child">
+            <h2>
+                <br><br><br><br><br>"We Believe Your Child <br> is a STAR" <br><br><br><br><br><br>
+            </h2>
+        </div>
+        <div class="testimonial-box">
+            <h2>Our Community</h2>
+            <p>HurleyPiano fosters connections among those with Special Needs...</p>
+            <img src="../img/about/community.png" alt="joining hands together in a pile"><br><br>
+            <h2>Innovative Ideas</h2>
+            <p>Innovation is synonymous with HurleyPiano...</p>
+            <img src="../img/about/ideas.png" alt="holding up a lightbulb to the sky"><br><br>
+            <h2>Our Website</h2>
+            <p>Our site embraces tech, primed for gadgets’ evolution...</p>
+            <img src="../img/about/our-website.png" alt="laptop on table"><br><br>
+        </div>
+        
+
+    </main>
+
+    <<?php include("footer.html") ?>
+
+    <!--Use the below code snippet to provide real time updates to the live chat plugin without the need of copying and paste each time to your website when changes are made via PBX-->
+    <call-us-selector phonesystem-url="https://1861.3cx.cloud" party="thienkimle"></call-us-selector>
+    
+    <!--Incase you don't want real time updates to the live chat plugin when options are changed, use the below code snippet. Please note that each time you change the settings you will need to copy and paste the snippet code to your website--> 
+    <!--<call-us 
+    phonesystem-url="https://1861.3cx.cloud" 
+    style="position:fixed;font-size:16px;line-height:17px;z-index: 99999;right: 0; bottom: 0;" 
+    id="wp-live-chat-by-3CX" 
+    minimized="true" 
+    animation-style="noanimation" 
+    party="thienkimle" 
+    minimized-style="bubbleright" 
+    allow-call="true" 
+    allow-video="false" 
+    allow-soundnotifications="true" 
+    enable-mute="true" 
+    enable-onmobile="true" 
+    offline-enabled="true" 
+    enable="true" 
+    ignore-queueownership="false" 
+    authentication="none" 
+    show-operator-actual-name="true" 
+    aknowledge-received="true" 
+    gdpr-enabled="false" 
+    message-userinfo-format="both" 
+    message-dateformat="both" 
+    lang="browser" 
+    button-icon-type="default" 
+    greeting-visibility="none" 
+    greeting-offline-visibility="none" 
+    chat-delay="2000" 
+    enable-direct-call="true" 
+    enable-ga="false" 
+    ></call-us>--> 
+    <script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -155,7 +155,7 @@
        </div>
 
         <div class="centered-tile-double">
-            <img src="img/special-needs-student.png" 
+            <img src="../img/special-needs-student.png" 
             alt="Specail needs students include ADHD, Asperger Syndrome, Autism, Down Syndrome, Dyscalculia, Dyselexia, Sensory Processing Disorder">
         </div>
         <!-- <form class="inquiry-form">
@@ -183,5 +183,41 @@
             <button type="submit">SUBMIT</button>
         </form> -->
     <?php include("footer.html") ?>
+
+<!--Use the below code snippet to provide real time updates to the live chat plugin without the need of copying and paste each time to your website when changes are made via PBX-->
+<call-us-selector phonesystem-url="https://1861.3cx.cloud" party="thienkimle"></call-us-selector>
+ 
+<!--Incase you don't want real time updates to the live chat plugin when options are changed, use the below code snippet. Please note that each time you change the settings you will need to copy and paste the snippet code to your website--> 
+<!--<call-us 
+phonesystem-url="https://1861.3cx.cloud" 
+style="position:fixed;font-size:16px;line-height:17px;z-index: 99999;right: 0; bottom: 0;" 
+id="wp-live-chat-by-3CX" 
+minimized="true" 
+animation-style="noanimation" 
+party="thienkimle" 
+minimized-style="bubbleright" 
+allow-call="true" 
+allow-video="false" 
+allow-soundnotifications="true" 
+enable-mute="true" 
+enable-onmobile="true" 
+offline-enabled="true" 
+enable="true" 
+ignore-queueownership="false" 
+authentication="none" 
+show-operator-actual-name="true" 
+aknowledge-received="true" 
+gdpr-enabled="false" 
+message-userinfo-format="both" 
+message-dateformat="both" 
+lang="browser" 
+button-icon-type="default" 
+greeting-visibility="none" 
+greeting-offline-visibility="none" 
+chat-delay="2000" 
+enable-direct-call="true" 
+enable-ga="false" 
+></call-us>--> 
+<script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
 </body>
 </html>
