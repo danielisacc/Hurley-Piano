57050fe35cd47a5243305072a9c82060f07c5ba3
lethienkim <131014498+lethienkim@users.noreply.github.com>
2024-07-10 03:38:37 -0500

    changes some format

--- about.php
+++ about.php
@@ -11,22 +11,29 @@
 <body>
     <?php include("header.html"); ?>
     <main>
-        
-    <!-- Testimonial Box 1 -->
+      
+    <!-- Testimonial Box 8 -->
     <div class="testimonial1-box">
-         
-            <img src="img/about/hero.png" alt="Hurley Piano Logo">
-            <p>
-            Hurley Piano specializes in developing educational methodologies that facilitate easy music learning, catering to parents teaching their children. Their expertise spans neuro-typical kids and those with special needs such as ADHD, Asperger Syndrome, Autism, Down Syndrome, Dyscalculia, Dyslexia, and Sensory Processing Disorder. </p>
-            <p>Richard Hurley has pioneered the acclaimed Black Keys Exercises, specifically crafted to introduce keyboard skills to even the most challenging special needs students. 
-            Based in Austin, Texas, a hub of special needs innovation, Hurley Piano collaborates with the city’s leading researchers to continually refine and validate their approach. They offer free introductory videos and blogs, fostering a video-based community discussion addressing the current challenges faced by special needs children, guided by their vision of inclusive music education...
-        </p>
+            <div class="star-child">
+                <h2>"We Believe Your Child is a STAR"</h2>
+                <p>
+                Hurley Piano specializes in creating educational methodologies that simplify music learning for parents teaching their children, including neuro-typical kids and those with special needs like ADHD, Asperger Syndrome, Autism, Down Syndrome, Dyscalculia, Dyslexia, and Sensory Processing Disorder. Richard Hurley has developed the acclaimed Black Keys Exercises to introduce keyboard skills to even the most challenging special needs students. Based in Austin, Texas, a hub of special needs innovation, Hurley Piano collaborates with leading researchers to refine their approach and offers free introductory videos and blogs, fostering a video-based community discussion to address the challenges faced by special needs children, all guided by their vision of inclusive music education.
+            </p>
+            </div>
+        </div>>
+ 
+
+    <!-- Testimonial Box 4 -->
+    <div class="testimonial-box">
+            <h2>My Mission</h2><br>
+            <img src="img/about/sheet-music.png" alt="fingers on sheet music">
+            <p>The Piano serves as a powerful educational tool for neurodiverse children...</p><br><br>
         </div>
 
         <!-- Testimonial Box 2 -->
     <div class="testimonial-box">
             <h2>My Vision</h2>
-            <img src="img/baby-piano-home.png" alt="baby play piano">
+            <img src="img/music-lessons/piano.png" alt="baby play piano">
             <p>
             Hurley Piano empowers every child, including those with special needs, through accessible and enriching music education...
             </p>
@@ -38,11 +45,11 @@
             <img src="img/about/piano.png" alt="child and adult playing piano together">
             <p>Hurley Piano develops education methodologies for easy learning to help parents teach their kids music...</p>
         </div>
-        <!-- Testimonial Box 4 -->
-        <div class="testimonial-box">
-            <h2>My Mission</h2><br>
-            <img src="img/about/sheet-music.png" alt="fingers on sheet music">
-            <p>The Piano serves as a powerful educational tool for neurodiverse children...</p><br><br>
+    <!-- Testimonial Box 6 -->
+    <div class="testimonial-box">  
+            <h2>Innovative Ideas</h2>
+            <img src="img/about/ideas.png" alt="holding up a lightbulb to the sky">
+            <p>Innovation is synonymous with HurleyPiano...</p><br><br>
         </div>
 
         <!-- Testimonial Box 5-->
@@ -51,26 +58,14 @@
             <img src="img/about/community.png" alt="joining hands together in a pile">
             <p>Hurley Piano fosters connections among those with Special Needs...</p><br><br>
         </div>
-         <!-- Testimonial Box 6 -->
-        <div class="testimonial-box">  
-            <h2>Innovative Ideas</h2>
-            <img src="img/about/ideas.png" alt="holding up a lightbulb to the sky">
-            <p>Innovation is synonymous with HurleyPiano...</p><br><br>
-        </div>
+         
          <!-- Testimonial Box 67-->
         <div class="testimonial-box">  
             <h2>Our Website</h2>
             <img src="img/about/our-website.png" alt="laptop on table">
             <p>Our site embraces tech, primed for gadgets’ evolution...</p><br><br>
         </div>
-    <!-- Testimonial Box 8 -->
-        <div class="testimonial-box">
-            <div class="star-child">
-            <h2>"We Believe Your Child is a STAR"</h2>
-            </div>         
-        </div>
-
-
+    
     </main>
 
     <?php include("footer.html"); ?>
