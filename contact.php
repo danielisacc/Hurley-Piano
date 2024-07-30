83aa88473b3061d2eae2048bc6455e08ef5f7f83
lethienkim <131014498+lethienkim@users.noreply.github.com>
2024-07-30 02:55:04 -0500

    correct the title

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Get In Touch</title>
+    <title>Hurley Piano Website - Contact Us</title>
     <link rel="icon" type="image/png" href="img/PianoLogo7.png">
     <link rel="stylesheet" href="contact.css">
 </head>
