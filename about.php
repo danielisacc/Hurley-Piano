3f6bd1198a21e4dd4c3609064b0cc93f5907cd22
lethienkim <131014498+lethienkim@users.noreply.github.com>
2024-08-02 11:06:28 -0500

    switch to Mr Hurley's 3CX acount.

--- about.php
+++ about.php
@@ -76,41 +76,8 @@ include "./scripts/config.php";
     </main>
 
     <?php include $html . "footer.html" ?>
-
 <!--Use the below code snippet to provide real time updates to the live chat plugin without the need of copying and paste each time to your website when changes are made via PBX-->
-<call-us-selector phonesystem-url="https://1861.3cx.cloud" party="thienkimle"></call-us-selector>
-
-<!--Incase you don't want real time updates to the live chat plugin when options are changed, use the below code snippet. Please note that each time you change the settings you will need to copy and paste the snippet code to your website--> 
-<!--<call-us 
-phonesystem-url="https://1861.3cx.cloud" 
-style="position:fixed;font-size:16px;line-height:17px;z-index: 99999;right: 0; bottom: 0;" 
-id="wp-live-chat-by-3CX" 
-minimized="true" 
-animation-style="noanimation" 
-party="thienkimle" 
-minimized-style="bubbleright" 
-allow-call="true" 
-allow-video="false" 
-allow-soundnotifications="true" 
-enable-mute="true" 
-enable-onmobile="true" 
-offline-enabled="true" 
-enable="true" 
-ignore-queueownership="false" 
-authentication="none" 
-show-operator-actual-name="true" 
-aknowledge-received="true" 
-gdpr-enabled="false" 
-message-userinfo-format="both" 
-message-dateformat="both" 
-lang="browser" 
-button-icon-type="default" 
-greeting-visibility="none" 
-greeting-offline-visibility="none" 
-chat-delay="2000" 
-enable-direct-call="true" 
-enable-ga="false" 
-></call-us>--> 
+<call-us-selector phonesystem-url="https://1888.3cx.cloud" party="hurleypiano"></call-us-selector>
 <script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
 
 <!-- UserWay widget script--> 
--- lessons.php
+++ lessons.php
@@ -119,9 +119,11 @@ include "./scripts/config.php";
     </main>
     <?php include $html . "footer.html" ?>
 
-    <call-us-selector phonesystem-url="https://1873.3cx.cloud/" party="hurleypiano"></call-us-selector>
-    <script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
+<!--Use the below code snippet to provide real time updates to the live chat plugin without the need of copying and paste each time to your website when changes are made via PBX-->
+<call-us-selector phonesystem-url="https://1888.3cx.cloud" party="hurleypiano"></call-us-selector>
+<script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
     
-    <script src="https://cdn.userway.org/widget.js" data-account="rmm88qREWe"></script>
+<!-- UserWay widget script  -->  
+<script src="https://cdn.userway.org/widget.js" data-account="rmm88qREWe"></script>
 </body>
 </html>
