0025a456059c5074e408a623732fd1be9f35b8b8
lethienkim <131014498+lethienkim@users.noreply.github.com>
2024-08-02 12:57:22 -0500

    nothing changes

--- lessons.php
+++ lessons.php
@@ -115,9 +115,44 @@
     </main>
     <?php include("footer.html"); ?>
 
-    <call-us-selector phonesystem-url="https://1873.3cx.cloud/" party="hurleypiano"></call-us-selector>
-    <script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
-    
-    <script src="https://cdn.userway.org/widget.js" data-account="rmm88qREWe"></script>
+<!--Use the below code snippet to provide real time updates to the live chat plugin without the need of copying and paste each time to your website when changes are made via PBX-->
+<call-us-selector phonesystem-url="https://1888.3cx.cloud" party="hurleypiano"></call-us-selector>
+ 
+<!--Incase you don't want real time updates to the live chat plugin when options are changed, use the below code snippet. Please note that each time you change the settings you will need to copy and paste the snippet code to your website--> 
+<!--<call-us 
+phonesystem-url="https://1888.3cx.cloud" 
+style="position:fixed;font-size:16px;line-height:17px;z-index: 99999;--call-us-main-accent-color:#47D6D6;--call-us-main-background-color:#FFFFFF;--call-us-plate-background-color:#206C77;--call-us-plate-font-color:#E6E6E6;--call-us-main-font-color:#292929;--call-us-agent-bubble-color:#29292910;right: 0; bottom: 0;" 
+id="wp-live-chat-by-3CX" 
+minimized="true" 
+animation-style="slidefromside" 
+party="hurleypiano" 
+minimized-style="bubbleright" 
+allow-call="true" 
+allow-video="false" 
+allow-soundnotifications="true" 
+enable-mute="true" 
+enable-onmobile="true" 
+offline-enabled="true" 
+enable="true" 
+ignore-queueownership="false" 
+authentication="none" 
+operator-name="Hurley Piano" 
+show-operator-actual-name="true" 
+aknowledge-received="true" 
+
+gdpr-enabled="true" 
+message-userinfo-format="both" 
+message-dateformat="both" 
+lang="browser" 
+button-icon-type="bubble" 
+greeting-visibility="none" 
+greeting-offline-visibility="none" 
+chat-delay="2000" 
+enable-direct-call="true" 
+enable-ga="false" 
+></call-us>--> 
+<script defer src="https://downloads-global.3cx.com/downloads/livechatandtalk/v1/callus.js" id="tcx-callus-js" charset="utf-8"></script>
+<!-- UserWay widget script  -->
+ <script src="https://cdn.userway.org/widget.js" data-account="rmm88qREWe"></script>
 </body>
 </html>
